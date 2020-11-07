346d178966308ddf50ed57154dfe89e982e104da
Ivàn Todorovich <ivan.todorovich@gmail.com>
2020-11-07 16:41:00 -0300

    [UPD] description

--- odoo_test_module/__manifest__.py
+++ odoo_test_module/__manifest__.py
@@ -1,5 +1,6 @@
 {
     'name': 'Odoo Test Uninstall Module',
+    'description': 'Demo module to test https://github.com/odoo/odoo/issues/61234',
     'version': "14.0.1.0.0",
     'author': 'Iván Todorovich',
     'license': 'OPL-1',
